3e13da8454cd960552b3a6945be09764b3b2039c
fingertyping <107344050+fingertyping@users.noreply.github.com>
2022-11-11 18:54:53 +0900

    Create Ring.js

---
+++ src/pages/Ring.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Seplayer from "../components/seplayer";
+import dig01 from "../sounds/Dig01-3L.mp3";
+
+class Ring extends Component {
+  render() {
+    return (
+      <div
+        className="Ring teal darken-1"
+        style={{
+          margin: "0",
+          padding: "3em",
+          minHeight: "100vh"
+        }}
+      >
+        <Seplayer file={dig01} />
+        <h1 className="white-text center">Ringing!</h1>
+      </div>
+    );
+  }
+}
+
+export default Ring;
